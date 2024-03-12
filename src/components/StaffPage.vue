a8613e152f91e6c8e5a31d33d07d51268d14af7f
chnmk <a2molchanov@ya.ru>
2024-03-12 18:43:57 +0300

    fix: move staff list layout to correct component

--- src/components/StaffPage.vue
+++ src/components/StaffPage.vue
@@ -1,31 +1,3 @@
 <template>
-  <div class="bg-white">
-    <h1 class="ma-2">
-      List
-    </h1>
-    <div class="d-inline-block">
-      <v-btn class="d-inline-block bg-grey rounded-xl pa-2 ma-2">
-        Button 1
-      </v-btn>
-      <v-btn class="d-inline-block bg-yellow rounded-xl pa-2 ma-2">
-        Button 2
-      </v-btn >
-      <v-btn class="d-inline-block bg-red rounded-xl pa-2 ma-2">
-        Button 3
-      </v-btn >
-      <v-btn class="d-inline-block bg-blue rounded-xl pa-2 ma-2">
-        Button 4
-      </v-btn >
-      <v-btn class="d-inline-block bg-green rounded-xl pa-2 ma-2">
-        Button 5
-      </v-btn >
-    </div>
-    <StaffList class="ma-2" />
-    <StaffList class="ma-2" />
-    <div class="ma-2">
-      <v-btn>
-        Show More
-      </v-btn>
-    </div>
-  </div>
+  <StaffList />
 </template>
