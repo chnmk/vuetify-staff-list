7a4ff30086aa434af32029a396d4760c1481e96f
chnmk <a2molchanov@ya.ru>
2024-03-12 18:56:58 +0300

    style: add basic page layout

--- src/components/StaffPage.vue
+++ src/components/StaffPage.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <StaffSearch />
-    <v-divider thickness="4" />
-    <StaffList />
-  </div>
+  <v-container :fluid="true" class="bg-grey pa-4">
+    <v-row>
+      <v-col>
+        <div class="bg-white ma-8 pa-4">
+          <StaffSearch />
+          <v-divider thickness="4" />
+          <StaffList />
+        </div>
+      </v-col>
+      <v-col>
+        <div class="bg-white ma-8 pa-4">
+          <NewUser />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
