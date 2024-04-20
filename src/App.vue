60a56b2bd72045fa10a68ae1ae51f2e11a61c9b2
chnmk <a2molchanov@ya.ru>
2024-04-20 19:20:18 +0300

    chore: initialize pinia

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,8 @@
 
 <script setup>
 import StaffPage from './components/StaffPage.vue';
+import { useTestStore } from '@/stores/test'
+
+const store = useTestStore()
+console.log(store.name)
 </script>
